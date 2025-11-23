c44690496f8ee1895e73f5177652fe98b96300c5
Amitraje9009 <rajeamit3535@gmail.com>
2025-11-23 19:55:54 +0530

    --

---
+++ streamapi/src/collection_class/Second.java
@@ -0,0 +1,15 @@
+package collection_class;
+
+public class Second {
+
+	public static void main(String[] args) {
+		Multiply<Integer> m1 = (t1,t2)->{return t1*t2;};
+			System.out.println(m1.multi(15,14));
+	}
+
+}
+
+interface Multiply<T>
+{
+	public T multi(T t1, T t2);
+}
