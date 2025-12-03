47e523e0ab106c20da11d140d40b3bcdf3bef15c
Amitraje9009 <rajeamit3535@gmail.com>
2025-12-03 15:06:07 +0530

    String Uniue

---
+++ streamapi/src/intermediatary_ops/String_unique.java
@@ -0,0 +1,55 @@
+package intermediatary_ops;
+
+import java.util.*;
+import java.util.function.Function;
+
+
+public class String_unique {
+
+	public static void main(String[] args) {
+		
+		String str ="ababcdeffgggghi";
+		
+		char[] chars=str.toCharArray();
+		
+		List<Character> list = new ArrayList<Character>();
+		
+		for(Character c:chars) {
+			list.add(c);
+		}
+		System.out.println(list);
+		
+		
+		
+		list.stream().distinct()
+		.map((i)->{
+			return i.toString().toUpperCase();
+		}).sorted(new Stringreverse()).forEach((j)->{System.out.println((j));});
+
+	}
+
+}
+
+
+class chartoString implements Function<Character, String>
+{
+
+	@Override
+	public String apply(Character t) {
+		 StringBuffer sb=new StringBuffer(t);
+		  System.out.println(sb.toString().toUpperCase());
+		 return sb.toString().toUpperCase();
+				 }
+	
+}
+
+class Stringreverse implements Comparator<String>
+{
+
+	
+	public int compare(String o1, String o2) {
+		
+		return o2.compareTo(o1);
+	}
+	
+}
