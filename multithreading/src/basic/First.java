576d3dc6b5b2ed58ab9370c36db7861365316b03
Amitraje9009 <rajeamit3535@gmail.com>
2026-01-16 21:21:00 +0530

    -

--- multithreading/src/basic/First.java
+++ multithreading/src/basic/First.java
@@ -2,9 +2,43 @@ package basic;
 
 public class First {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		
+		boolean status = true;
+		
+		Number_Printer n1 = new Number_Printer();
+		Number_Printer n2 = new Number_Printer();
+		Number_Printer n3 = new Number_Printer();
+		Number_Printer n4 = new Number_Printer();
 
+		
+			n1.start();
+			n2.start();
+			n3.start();
+			n4.start();
+			
+			while(status) {
+		Thread.sleep(1000);
+		System.out.println("*");
+		}
+			
+			throw new Exception("demo");
 	}
 
+}
+
+class Number_Printer extends Thread
+{
+	
+	public void run() {
+		for (char i = 'A';i<'Z';i++) {
+			System.out.println(i);
+			try {
+				Thread.sleep(1000);
+			} catch (Exception e) {
+				System.out.println(e.getMessage());
+			}
+		}
+	}
+	
 }
--- multithreading/src/basic/Third.java
+++ multithreading/src/basic/Third.java
@@ -2,18 +2,56 @@ package basic;
 
 public class Third {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
-	}
+
+		
+		Runnable r=()->{
+
+			for(int i=0;i<15;i++)
+			{
+				System.out.println("--!--");
+				try {
+					Thread.sleep(500);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		};
+
+		
+		Thread th=new Thread(r); 
+		
+		th.start();
+		th.join();
+		for(int i=0;i<10;i++)
+		{
+			System.out.println("*");
+			Thread.sleep(1000);
+		}
+		
+		}
 
 }
-
-class Number implements Runnable{
-
-
-	public void run() {
-//		for(i)
-		
-	}
-	
-}
+//
+//class Number implements Runnable
+//{
+//
+//	@Override
+//	public void run()  {
+//		
+//		for(int i=0;i<100;i++)
+//		{
+//			System.out.println(i);
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//				// TODO Auto-generated catch block
+//				e.printStackTrace();
+//			}
+//		}
+//		
+//	}
+//
+//}
